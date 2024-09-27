53bcff6a51e5f8084576aaf3650144920c99ca96
Renjini Rajeev <71512100+renjinirajeev@users.noreply.github.com>
2024-09-27 13:06:02 +0530

    Add files via upload

---
+++ src/test/java/com/automation/pages/BasePage.java
@@ -0,0 +1,27 @@
+package com.automation.pages;
+
+import com.automation.utils.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    WebDriver driver;
+
+    public BasePage(){
+        this.driver = DriverManager.getDriver();
+        PageFactory.initElements(driver, this);
+
+    }
+
+    public boolean isDisplay(WebElement element){
+        try {
+            return  element.isDisplayed();
+        }catch (Exception e){
+            return false;
+        }
+
+    }
+
+}
